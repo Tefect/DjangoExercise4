273a81be4696002676cf640d491bf83f773c363b
Tefect <tendai6jmcs@gmail.com>
2026-04-13 23:55:35 -0400

    Fix ALLOWED_HOSTS for EC2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^1tuiurlj4#a*g!5#e*5s+tjlmnru9q6@hqpmz+5(od$lsshe^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.16.1.1', 'localhost', '127.0.0.1']
 
 
 # Application definition
